84414a48b5d9e6be5c4a2e80d067440111cf1e40
Annette <annette.tikhonova@gmail.com>
2014-10-29 13:07:30 +0400

    graphs

--- wordcloud.R
+++ wordcloud.R
@@ -19,19 +19,28 @@ library("RColorBrewer")
 #          use.r.layout=FALSE, colors=brewer.pal(8, "Dark2"))
 #wordcloud(camnew)
 
-d <- data_frame(
+d <- data.frame(
   word=c("Эконометрика","R","ВШЭ","гетероскедастичность","автокорреляция",
-         "робастные ошибки","P-значение", "тест", "теорема Гаусса-Маркова")
+         "робастные ошибки","P-значение", "тест", "Гаусс", "Марков",
+         "мультиколлинеарность","гипотеза", "стохастический", "регрессор",
+         "стандартное отклонение", "данные","МНК","логит",
+         "коэффициент детерминации","регрессия","предельные эффекты","стационарность",
+         "инструменты","остатки")
   )
 
 # выдумываем частоты для слов
-d$freq <- sample(8:10,size = nrow(d),replace = TRUE) 
-d$freq[1:3] <- c(20,18,16) # первые три слова крупно
+d$freq <- sample(5:10,size = nrow(d),replace = TRUE) 
+d$freq[1:3] <- c(20,18,14) # первые три слова крупно
 
-pal <- brewer.pal(9, "BuGn")
+pal <- brewer.pal(9, "Set1")
+pal <- c("#0000FF","#FF6600","#00CC33","#FFFF00","","","","")
+
+pal <- c(brewer.pal(9, "Blues")[5:9], brewer.pal(9, "Oranges")[4:9], 
+         brewer.pal(9, "Greens")[5:9],brewer.pal(9, "GnBu")[5:9], 
+         brewer.pal(9, "Reds")[5:9], brewer.pal(9, "Purples")[5:9])
 
 wordcloud(d$word,d$freq, scale=c(3,.1),min.freq=2,max.words=100, 
-          random.order=T, rot.per=.15, colors=pal) 
+          random.order=T, rot.per=0, colors=pal, fixed.asp=TRUE) 
 
 wordcloud(d$word,d$freq, random.order=T, rot.per=.15, colors=pal) 
 
