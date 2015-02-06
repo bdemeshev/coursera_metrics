a9eb6f24bea33f1150b0012e72c24518cf436048
Annette <annette.tikhonova@gmail.com>
2015-02-06 15:37:59 +0400

    graphs

--- quiz2.R
+++ quiz2.R
@@ -13,6 +13,18 @@ summary(model2)
 model3 <- lm(data=data, price ~ carat+x+y)
 summary(model3)
 
+model_ln <- lm(data=data, log(price) ~ carat)
+summary(model_ln)
+
+model_ln1 <- lm(data=data, price ~ log(carat))
+summary(model_ln1)
+
+model_ln2 <- lm(data=data, log(price) ~ log(carat))
+summary(model_ln2)
+
+model_d <- glm(data=data, price ~ carat + color)
+summary(model_d)
+
 10117.99-62.56*1.645
 10117.99+62.56*1.645
 
@@ -22,17 +34,85 @@ summary(model3)
 -1148.61-36.31*1.645
 -1148.61+36.31*1.645
 
-150-105
+# Res 
+200-105
+# Unres 
 200-175
 
-(45-25)*(25-5)/(2*25)
+(95-25)*(25-5)/(2*25)
 
-190-125
+200-125
 200-175
 
-(65-25)*(40-5)/(2*25)
+(75-25)*(40-5)/(2*25)
 
-170-130
-200-180
+210-130
+210-180
 
-(40-20)*(35-5)/(2*20)
+(80-30)*(35-5)/(2*30)
+
+h <- read.table("flats_moscow.txt", header=TRUE, sep = "\t", dec = ".")
+glimpse(h)
+
+model <- lm(data=h, price~livesp)
+vcov(model)
+coef(model)
+
+V <- matrix(c(21.9,-0.46,-0.46,0.01),nrow=2)
+V
+
+s2 <- deviance(model)/(nrow(h)-2)
+s2
+
+library("dplyr")
+x <- c(1,150)
+
+t(x) %*% V %*% x
+t(x) %*% V %*% x + s2
+
+
+
+19+(100^2)*0.01+(80^2)*0.03+2*0.03*100+2*(-0.02)*100*80+2*80*(-0.45)
+19+(90^2)*0.01+(70^2)*0.03+2*0.03+2*(-0.02)+2*(-0.45)
+19+(80^2)*0.01+(50^2)*0.03+2*0.03+2*(-0.02)+2*(-0.45)
+
+900+310.12
+900+246.12
+900+157.12
+
+library(memisc)
+library(lmtest)
+model1 <- lm(data=data, price ~ carat)
+summary(model1)
+model2 <- lm(data=data, price ~ carat + depth)
+model3 <- lm(data=data, price ~ carat + depth + cut)
+
+mtable(model1, model2, model3)
+
+resettest(model1)
+resettest(model2)
+resettest(model3)
+
+qplot(data = data, log(price))
+qplot(data = data, log(price), fill=cut)
+qplot(data = data, log(price), fill=cut, position = "dodge")
+qplot(data = data, log(price), fill=cut, geom = "density", alpha = 0.5) + facet_grid(~cut)
+
+qplot(data = data, log(price), fill=clarity, geom = "density", alpha = 0.5) + facet_wrap(~clarity)
+qplot(data = data, log(price), fill=clarity, geom = "density", alpha = 0.5) + facet_grid(~clarity)
+
+qplot(data = data, log(price), fill=color, geom = "density", alpha = 0.5) + facet_wrap(~color)
+
+
+waldtest(model1, model2)
+waldtest(model1, model3)
+waldtest(model2, model3)
+
+qplot(data=data, log(carat), log(price), color = clarity) + facet_wrap(~cut) 
+
+round(pnorm(9, mean=7, sd=2), digits = 2)
+
+0.2*2+0.2*4
+1-(70/7)/(100/9)
+1-(9/18)/(100/20)
+1-(50/5)/(100/(7))
