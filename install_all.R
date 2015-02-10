87be6b8a9a030a342ac262a0ddbc523886c9b8f6
Boris Demeshev <boris.demeshev@gmail.crom>
2015-02-10 10:50:52 +0300

    install all

--- install_all.R
+++ install_all.R
@@ -1,6 +1,6 @@
 
-library("bstats") # больше тестов????????
-library("rusquant") # загрузка с finam.ru ?????????
+# обойтись без bstats через bptest в жанре:
+# bptest(cig_lm2, ~ income * price + I(income^2) + I(price^2), data = CigarettesB)
 
 
 
@@ -17,6 +17,7 @@ install.packages("AUC")
 
 install.packages("ggplot2")
 install.packages("GGally")
+install.packages("lattice")
 install.packages("vcd")
 install.packages("hexbin")
 install.packages("sjPlot")
@@ -27,23 +28,16 @@ install.packages("broom")
 install.packages("tidyr")
 install.packages("psych")
 
-
-
-
 install.packages("glmnet")
 install.packages("HSAUR")
 install.packages("sgof")
 install.packages("car")
 
-
-
 install.packages("spikeslab")
 install.packages("quantreg")
 install.packages("MCMCpack")
 
-
 install.packages("devtools")
-
 
 install.packages("caret")
 install.packages("AER")
@@ -57,7 +51,9 @@ install.packages("forecast")
 install.packages("quantmod") # загрузка с finance.google.com
 install.packages("Quandl") # загрузка с Quandl
 
-
+# non-CRAN packages:
 
 devtools::install_github("bdemeshev/rlms") # read RLMS data
 devtools::install_github("bdemeshev/sophisthse") # read data from sophist.hse.ru
+
+install.packages("rusquant",repos="http://r-forge.r-project.org", type="source")
