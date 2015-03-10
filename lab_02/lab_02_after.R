bc0e84024b324dfb2ea73451f0594f3626cbe6e9
Boris Demeshev <boris.demeshev@gmail.com>
2015-03-10 12:33:36 +0300

    video comments 4 round 2

--- lab_02/lab_02_after.R
+++ lab_02/lab_02_after.R
@@ -1,7 +1,6 @@
 # if you see KRAKOZYABRY then do 
 # "File" - "Reopen with encoding" - "UTF-8" - (Set as default) - OK
 
-
 # lab 2
 
 # загружаем пакеты
@@ -67,76 +66,91 @@ confint(model) # доверительные интервалы для коэфф
 sjp.lm(model) # графическое представление интервалов
 
 # проверка гипотезы b_Cath=b_Agri
-
-model_aux <- lm(data=h, 
-      Fertility~Catholic+I(Catholic+Agriculture)+Examination)
+# построение вспомогательной модели
+model_aux <- lm(data=h, Fertility~Catholic+I(Catholic+Agriculture)+Examination)
 summary(model_aux)
 
+# проверка гипотезы без построения вспомогательной модели
 linearHypothesis(model, "Catholic-Agriculture=0")
 
 # стандартизированные коэффициенты
 
+# масштабируем каждую переменную (вычитаем среднее, делим на стандартную ошибку)
 h_st <- mutate_each(h, "scale")
-glimpse(h_st)
-model_st <- lm(data=h_st, 
-               Fertility~Catholic+Agriculture+Examination)
-summary(model_st)
-sjp.lm(model, showStandardBeta = TRUE)
+glimpse(h_st) # смотрим на новый набор данных
+# оцениваем модель по стандартизированным данным
+model_st <- lm(data=h_st, Fertility~Catholic+Agriculture+Examination)
+summary(model_st) # отчет о новой модели
+sjp.lm(model, showStandardBeta = TRUE) # графическое представление
 
 
 # искусственный эксперимент
 
+# матрица в 100 строк, слепленная из вектора в котором 4100 элементов
 D <- matrix(nrow=100, rnorm(100*41,mean=0,sd=1))
-df <- data.frame(D)
+df <- data.frame(D) # переводим матрицу в табличку данных 
 glimpse(df)
 
-model_pusto <- lm(data=df, X1~. )
+# пытаемся определить зависимость X1 от остальных переменных
+model_pusto <- lm(data=df, X1~. ) 
 summary(model_pusto)
 
-# сравнить несколько моделей
-
+# сравниваем несколько моделей
 model2 <- lm(data=h, Fertility~Catholic+Agriculture)
 compar_12 <- mtable(model, model2)
 compar_12
 
 # сохранение результатов работы
-stuff <- list(data=h, model=model2)
-saveRDS(file = "mydata.RDS",stuff)
+stuff <- list(data=h, model=model2) # список ценных объектов
+saveRDS(file = "mydata.RDS",stuff) # сохраняем всё ценное в файл
 
-mylist <- readRDS("mydata.RDS")
+mylist <- readRDS("mydata.RDS") # читаем из файла что там есть
 summary(mylist$model)
 
-# csv. comma separated values
+# формат csv. comma separated values
 
+# первая (сознательно неудачная) попытка чтения файла
 t <- read.csv("flats_moscow.txt")
 glimpse(t)
 
+# попытка чтения файла с указанием параметров:
 t <- read.csv("flats_moscow.txt", sep="\t", dec=".", header=TRUE)
 glimpse(t)
 
+# простая модель зависимости цены от общей площади и кирпичности дома
 mod_3 <- lm(data=t, price~totsp+brick)
 summary(mod_3)
 
-# RLMS 
+# читаем данные RLMS 
 
-df <- read.spss("r21i_os24a.sav", to.data.frame=TRUE,
-                reencode="UTF-8")
-glimpse(df)
+# предварительно их нужно скачать с http://www.hse.ru/rlms/spss
+# для примера взята 21 волна, индивиды, репрезентативная выборка
 
-df_sel <- select(df, qm1, qm2, qh6)
-glimpse(df_sel)
+# командой из пакета rlms
+h <- read.rlms("r21i_os24a.sav")
+# если пакет rlms почему-то не установился, то 
+# нужно убрать комментарий и выполнить следующую строку:
+# h <- read.spss("r21i_os24a.sav", to.data.frame=TRUE, reencode="UTF-8")
+glimpse(h)
 
-describe(df_sel)
+# отбираем часть переменных из таблички h в табличку h2
+h2 <- select(h, qm1, qm2, qh6, qh5)
+glimpse(h2) # смотрим на df_sel
 
-df2 <- mutate(df_sel,
-              age=2012-qh6,
-              ves=ifelse(qm1>10^6,NA,qm1),
-              rost=ifelse(qm2>10^6,NA,qm2))
-describe(df2)
+# переименовываем переменные
+h3 <- rename(h2, ves=qm1, rost=qm2, sex=qh5, b_year=qh6)
+# добавляем возраст
+h3 <- mutate(h3, vozrast=2012-b_year)
 
-qplot(data=df2, x=rost, y=ves)
-qplot(data=df2, x=rost, y=ves) + geom_hex()
+describe(h3) # описательные статистики
 
-df3 <- select(df2, rost, ves, age)
-df4 <- filter(df3, age>50)
-qplot(data=df4, x=rost, y=ves) + geom_hex()
+summary(h3$sex) # таблица частот
+
+# отберём мужчин в отдельную табличку
+h4 <- filter(h3, sex=="МУЖСКОЙ")
+
+# диаграмма рассеяния
+qplot(data=h4, rost, ves)
+
+# гистограмма
+qplot(data=h4, ves)
---
+++ lab_02/lab_02_before.R
@@ -0,0 +1,17 @@
+# if you see KRAKOZYABRY then do 
+# "File" - "Reopen with encoding" - "UTF-8" - (Set as default) - OK
+
+# lab 2
+
+# загружаем пакеты
+library("memisc") # две и более регрессий в одной табличке
+library("dplyr") # манипуляции с данными
+library("psych") # описательные статистики
+library("lmtest") # тестирование гипотез в линейных моделях
+library("sjPlot") # графики
+library("sgof")
+library("ggplot2") # графики
+library("foreign") # загрузка данных в разных форматах
+library("car")
+library("hexbin") # графики
+library("rlms") # загрузка данных в формате rlms (spss)
