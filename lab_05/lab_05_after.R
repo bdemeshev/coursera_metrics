b4f797ff6b863e9567c3e8e09493f641a7fbb7fa
Boris Demeshev <boris.demeshev@gmail.com>
2015-03-18 12:54:14 +0300

    lab 05 after new :)

--- lab_05/lab_05_after.R
+++ lab_05/lab_05_after.R
@@ -1,23 +1,17 @@
-# library("devtools")
-# install_github("dgrtwo/broom")
-# install_github("cran/bstats")
-
 library("sandwich") # vcovHC, vcovHAC
 library("lmtest") # тесты
 library("car") # еще тесты
-library("bstats") # больше тестов
 library("dplyr") # манипуляции с данными
 library("broom") # манипуляции
 library("ggplot2") # графики
 
-# гетероскедастичность
-h <- read.table("flats_moscow.txt",header=TRUE)
+h <- read.table("flats_moscow.txt", header=TRUE)
 head(h)
 tail(h)
 
-qplot(data=h,x=totsp, y=price)
+qplot(data=h, x=totsp, y=price)
 
-model <- lm(price~totsp,data=h)
+model <- lm(price~totsp, data=h)
 summary(model)
 coeftest(model)
 confint(model)
@@ -45,29 +39,13 @@ ci
 
 confint(model)
 
-# White test
-white.test(model)
-# H0: Var(epsilon_i)=const
-res <- white.test(model)
-res$statistic
-res$p.value
+bptest(model)
+bptest(model, data=h, varformula = ~ totsp + I(totsp^2) )
+bptest(model, data=h, varformula = ~ poly(totsp, 2) )
 
-# GQ test
-h <- arrange(h,totsp)
-model <- lm(price~totsp,data=h)
-gqtest(model,fraction = 0.20)
-# H0: Var(epsilon_i)=const
-# H0 отвергается
+gqtest(model, order.by = ~totsp, data=h, fraction = 0.2)
 
-qplot(data=h,totsp,price)
-h <- mutate(h, ln_totsp = log(totsp), ln_price=log(price))
-qplot(data=h,ln_totsp,ln_price)
+qplot(data=h, log(totsp), log(price))
 
-model2 <- lm(data=h, ln_price~ln_totsp)
-white.test(model2)
-white.test(model)
-gqtest(model2)
-gqtest(model)
-
-summary(model)
-summary(model2)
+model2 <- lm(data=h, log(price)~log(totsp))
+gqtest(model2, order.by = ~totsp, data=h, fraction = 0.2)
