c1671aa3c2f62b9aefbf03c9c362cf1ad7ae8999
Boris Demeshev <boris.demeshev@gmail.com>
2015-03-18 12:54:32 +0300

    lab 05 before small

---
+++ lab_05/lab_05_before.R
@@ -0,0 +1,8 @@
+
+library("sandwich") # vcovHC, vcovHAC
+library("lmtest") # тесты
+library("car") # еще тесты
+library("dplyr") # манипуляции с данными
+library("broom") # манипуляции
+library("ggplot2") # графики
+
