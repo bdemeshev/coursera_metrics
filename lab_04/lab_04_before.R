818e2877739e0f423a62cfb88305e73a6350ca03
Boris Demeshev <boris.demeshev@gmail.com>
2015-03-22 12:41:17 +0300

    04 before/after

---
+++ lab_04/lab_04_before.R
@@ -0,0 +1,11 @@
+# ESLI RUSSKIE BUKVI NE VIDNI --->
+# File -- Reopen with encoding --- utf8 --- set as default --- ok
+
+
+library("HSAUR") # из этого пакета возьмем набор данных по семиборью
+library("dplyr") # манипуляции с данными
+library("psych") # описательные статистики
+library("lmtest") # тесты для линейных моделей
+library("glmnet") # LASSO + ridge
+library("ggplot2") # графики
+library("car") # vif
