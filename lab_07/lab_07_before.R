48e458e4820a1009b5f33fec079e92fc63e8074a
Boris Demeshev <boris.demeshev@gmail.com>
2015-04-12 22:59:42 +0300

    lec 07, 08 and lab 07 08

--- lab_07/lab_07_before.R
+++ lab_07/lab_07_before.R
@@ -1,13 +1,20 @@
-# lab_07
+# Esli russkie bukvi prevratilitis v krakozyabry,
+# to File - Reopen with encoding... - UTF-8 - Set as default - OK
 
-library("dplyr")
-library("erer")
-library("vcd")
-library("ggplot2")
-library("reshape2")
-library("AUC")
+# lab 07
 
+library("dplyr") # манипуляции с данными
+library("erer") # расчет предельных эффектов
+library("vcd") # графики для качественных данных
+library("ggplot2") # графики
+library("reshape2") # манипуляции с данными
+library("AUC") # для ROC кривой
+
+# при загрузке файлов R автоматом переделывает все строковые переменные в факторные
+# эта шаманская команда просит R так не делать :)
 options(stringsAsFactors=FALSE)
 
+# читаем данные по пассажирам Титаника
 t <- read.csv("titanic3.csv")
+# источник и описание:
 # http://lib.stat.cmu.edu/S/Harrell/data/descriptions/titanic.html
