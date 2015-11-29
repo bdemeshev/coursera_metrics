d3b8ed79e31ee980df388fcd00e100ff22ce33df
Boris Demeshev <boris.demeshev@gmail.com>
2015-11-29 14:35:16 +0300

    num ready? :)

--- coursera2openedu/coursera2openedu.R
+++ coursera2openedu/coursera2openedu.R
@@ -1,4 +1,4 @@
-# read coursera test xml and convert it to markdown openedu
+# read coursera test xml and convert it to markdown/xml edx (openedu)
 library("XML")
 library("stringr")
 library("dplyr")
@@ -69,6 +69,21 @@ GetVersion <- function(doc, question_no, version_no) {
   return(version)
 }
 
+
+# get option group from version
+GetOptionGroup <- function(version, option_group_no) {
+  option_group <- version[["data"]][["option_groups"]][[option_group_no]]
+  return(option_group)
+}
+
+# get option from version
+GetOption <- function(version, option_group_no, option_no) {
+  option_group <- GetOptionGroup(version, option_group_no)
+  option <- option_group[[option_no]]
+  return(option)
+}
+
+
 # get child names of an xml node
 ChildNames <- function(node) {
   return(XML::xmlSApply(node, XML::xmlName))
@@ -109,19 +124,21 @@ GetQExplanation(v_num)
 
 # get answer from numeric question (we assume it's unique!!!)
 GetNumAnswer <- function(version) {
-  answer <- XML::xmlValue(version[["data"]][["option_groups"]][["option_group"]][["option"]][["text"]])
+  option <- GetOption(version, 1, 1)
+  answer <- XML::xmlValue(option[["text"]])
   return(answer)
 }
 
 # get point explanation from numeric question
 GetNumPointExplanation <- function(version) {
-  point_explanation <- xmlValue(version[["data"]][["option_groups"]][["option_group"]][["option"]][["explanation"]])
+  option <- GetOption(version, 1, 1)
+  point_explanation <- xmlValue(option[["explanation"]])
   return(point_explanation)
 }
 
 # remove <br> tags (edx does not support them)
 # replace each <br> with <p> ... </p>
-nobr <- function(output_string) {
+BRtagToPtag <- function(output_string) {
   output_string <- paste0("<p>", output_string, "</p>")
   output_string <- str_replace_all(output_string, "<br>", "</p>\n<p>")
   return(output_string)
@@ -134,66 +151,93 @@ question_explanation <- GetQExplanation(v_num)
 answer <- GetNumAnswer(v_num)
 point_explanation <- GetNumPointExplanation(v_num)
 
+# this function converts inline equations 
+# $$x^2$$ to \(x^2\)
+DollarsToBrackets <- function(output_string) {
+  bracket_pairs <- str_count(output_string, pattern = "[$][$]") / 2
+  for (index in 1:bracket_pairs) {
+    output_string <- str_replace(output_string, 
+                                 pattern = "[$][$]", replacement = "\\\\(")
+    output_string <- str_replace(output_string, 
+                                 pattern = "[$][$]", replacement = "\\\\)")
+  }
+  return(output_string)
+}
 
 # abandoned function. should make markdown questions
 Version2edx <- function(version) {
-  text <- GetText(version) %>% dd2brackets()
+  text <- GetText(version) %>% DollarsToBrackets()
   output <- paste0(">>", text, "<<\n")
-  
+  # ... to much to do :)
   return(output)
 }
 cat(Version2edx(v_num))
 a <- Version2edx(v_num)
 a
 
-# this function converts inline equations 
-# $$x^2$$ to \(x^2\)
-dd2brackets <- function(output_string) {
-  bracket_pairs <- str_count(output_string, pattern = "[$][$]") / 2
-  for (index in 1:bracket_pairs) {
-    output_string <- str_replace(output_string, 
-      pattern = "[$][$]", replacement = "\\\\(")
-    output_string <- str_replace(output_string, 
-      pattern = "[$][$]", replacement = "\\\\)")
+
+cat(DollarsToBrackets(a))
+
+# create edx xml of numeric question from prepared nodes
+CreateNumFromNodes <- function(
+  text_nodes,
+  point_explanation_nodes,
+  answer,
+  question_explanation_nodes,
+  hints_nodes_list = NULL) {
+  
+  response_node <- xmlNode("numericalresponse", 
+                           attrs = c(answer = answer),
+                           xmlNode("formulaequationinput"),
+                           xmlNode("correcthint", 
+                                   .children = point_explanation_nodes))
+  
+  solution_node <- xmlNode("solution", 
+                           xmlNode("div", 
+                                   attrs = c(class="detailed-solution"), 
+                                   .children = question_explanation_nodes))
+  
+  
+  problem_node <- xmlNode("problem", .children = text_nodes)
+  problem_node <- addChildren(problem_node, response_node, solution_node)
+  
+  if (!is.null(hints_nodes_list)) {
+    demandhint_node <- xmlNode("demandhint")
+    for (i in 1:length(hints)) {
+      demandhint_node <- addChildren(demandhint_node, 
+                                     xmlNode("hint", 
+                                             .children = hints_nodes_list[[i]]))
+    }
+    problem_node <- addChildren(problem_node, demandhint_node)
   }
-  return(output_string)
+  return(problem_node)
 }
-cat(dd2brackets(a))
 
 
 
-
-# create edx xml
-
-
-CreateNumeric <- function(
+# create edx xml of numeric question from plain text
+CreateNumFromText <- function(
   text = "Кому на Руси жить хорошо?",
   point_explanation = "here is the hint",
   answer = 4,
   question_explanation = "Тому, кто досмерти работает, до полусмерти пьет",
   hints = c("hint раз", "hint 2")) {
   
-  response_node <- xmlNode("numericalresponse", 
-                           attrs = c(answer = answer),
-                           xmlNode("formulaequationinput"),
-                           xmlNode("correcthint", point_explanation))
+  # transfrom plain text to xml <p> nodes taking care of <br> tag
+  text_nodes <- TextToNodes(text)
+  question_explanation_nodes <- TextToNodes(question_explanation)
+  point_explanation_nodes <- TextToNodes(point_explanation)
   
-  solution_node <- xmlNode("solution", 
-                           xmlNode("div", attrs = c(class="detailed-solution"), 
-                                   question_explanation))
+  hints_nodes_list <- TextToNodes(hints)
   
   
-  problem_node <- xmlNode("problem", text, 
-                          response_node, solution_node)
+  problem_node <- CreateNumFromNodes(text_nodes = text_nodes, 
+                                     point_explanation_nodes = point_explanation_nodes, 
+                                     answer = answer, 
+                                     question_explanation_nodes = question_explanation_nodes, 
+                                     hints_nodes_list = hints_nodes_list)
   
-  if (!is.null(hints)) {
-    demandhint_node <- xmlNode("demandhint")
-    for (i in 1:length(hints)) {
-      demandhint_node <- addChildren(demandhint_node, 
-                                     xmlNode("hint", hints[i]))
-    }
-    problem_node <- addChildren(problem_node, demandhint_node)
-  }
+  
   return(problem_node)
 }
 
@@ -238,10 +282,7 @@ VersionSummary <- function(doc) {
   return(v_summary)
 }
 
-GetOptionGroup <- function(version, option_group_no) {
-  option_group <- version[["data"]][["option_groups"]][[option_group_no]]
-  return(option_group)
-}
+
 
 # summary on option groups
 OptionGroupSummary <- function(doc) {
@@ -283,23 +324,65 @@ GetTotalNumberOfOptions(v_checkbox)
 og <- OptionGroupSummary(doc)
 v <- VersionSummary(doc)
 
-# remove <br>, transform $$...$$ to \( ... \)
-ddbr <- function(input_string) {
-  output_string <- nobr(dd2brackets(input_string))
-  return(output_string)
-}
+
 
 
 GetNumberOfOptionGroups(v_radio)
 
 
+# text may have <br> <p> </p> tags
+# edx does not support <br> tag
+# TextToNodes0 transforms all text between <br> into separate <p> xml nodes
+# to avoid possible nesting of <p> ... </p> with existing ones
+# TextToNodes0 transforms all </p> to <br> and removes all <p> beforehand
+# replace_dollars option --- transform $$...$$ to \(...\)
+TextToNodes0 <- function(input_text, node_name = "p", replace_dollars = TRUE) {
+  if (is.null(input_text)) {
+    return(NULL)
+  }
+  
+  if (replace_dollars) {
+    input_text <- DollarsToBrackets(input_text)
+  }
+  
+  # remove <p>
+  output_text <- stringr::str_replace_all(input_text, "<p>", "")
+  # replace </p> with <br>
+  output_text <- stringr::str_replace_all(output_text, "</p>", "<br>")
+  
+  fragments <- stringr::str_split(output_text, "<br>")[[1]]
+  
+  # add <p> ... </p>
+  fragments <- paste0("<p>", fragments, "</p>")
+  
+  list_of_nodes <- list()
+  for (i in 1:length(fragments)) {
+    parsed_fragment <- XML::xmlTreeParse(file = fragments[i], asText = TRUE)
+    list_of_nodes[[i]] <- XML::xmlRoot(parsed_fragment)
+  }
+  
+  return(list_of_nodes)
+}
+
+# the same as TextToNodes0, but vectorized
+# if vector_input is of length 1 returns a list of nodes
+# if vector_input is of length > 1 returns a list of list of nodes
+TextToNodes <- function(vector_input) {
+  if (length(vector_input) < 2) { # 0 (if NULL) and 1 are ok
+    nodes_list <- TextToNodes0(vector_input)
+  } else {
+    nodes_list <- list()
+    for (i in 1:length(vector_input)) {
+      nodes_list[[i]] <- TextToNodes0(vector_input[i])
+    }
+  }
+  
+  return(nodes_list)
+}
+
 # 
 TransformNumeric <- function(version) {
-  text <- GetText(version) %>% ddbr()
-  answer <- GetNumAnswer(version) %>% ddbr()
-  question_explanation <- GetQExplanation(version) %>% ddbr()
-  point_explanation <- NULL
-  hints <- NULL
+
   
   # Check
   type <- GetType(version)
@@ -315,13 +398,59 @@ TransformNumeric <- function(version) {
     warning("The question should have one option in total, but has = ", tot_n_options)
   }
   
+  # obtain plain text 
+  text <- GetText(version) 
+  question_explanation <- GetQExplanation(version) 
+  
+  answer <- GetNumAnswer(version) 
+  point_explanation <- GetNumPointExplanation(version)
+  
+  hints <- NULL
   
   
-  edx_question <- CreateNumeric(text = text, answer = answer, 
-      hints = NULL, question_explanation = question_explanation,
-      point_explanation = point_explanation)
+  # transfrom plain text to xml <p> nodes taking care of <br> tag
+  text_nodes <- TextToNodes(text)
+  question_explanation_nodes <- TextToNodes(question_explanation)
+  point_explanation_nodes <- TextToNodes(point_explanation)
+  
+  hints_nodes_list <- TextToNodes(hints)
+  
+  
+  edx_question <- CreateNumFromNodes(text_nodes = text_nodes, 
+                  point_explanation_nodes = point_explanation_nodes, 
+                  answer = answer, 
+                  question_explanation_nodes = question_explanation_nodes, 
+                  hints_nodes_list = hints_nodes_list)
   return(edx_question)
 }
 
 TransformNumeric(v_num)
+
+
+Coursera2edx <- function(doc, wd = getwd()) {
+  
+  message("Questions saved in folder: ", wd)
+  
+  n_questions <- GetNumberOfQuestions(doc)
+  test_title <- GetTitle(doc)
+  test_preamble <- GetPreamble(doc)
+  
+  message("Test: ", test_title)
+  message("Number of questions: ", n_questions)
+  
+  for (q_no in 1:n_questions) {
+    n_versions <- GetNumberOfVersions(doc, question_no = q_no)
+    message("Processing question ", q_no,". Number of versions: ", n_versions)
+    for (v_no in 1:n_versions) {
+      version <- GetVersion(doc, q_no, v_no)
+      v_type <- GetType(version)
+      
+      message("Question: ", q_no, ". Version: ", v_no, ". Type: ", v_type, ".")
+    }
+  }
+}
+
+a <- GetText(GetVersion(doc, 17, 1))
+a
+
 
